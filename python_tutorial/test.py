05a4a398ab05e4838cc79769e6ab44ae362b10c5
parul100495 <parul100495@gmail.com>
2020-09-22 00:09:38 -0400

    Addition of bound propagation and week 3 report

--- python_tutorial/test.py
+++ python_tutorial/test.py
@@ -4,7 +4,7 @@ from equation_parser import *
 
 if __name__ == "__main__":
     # test 1
-    rev_polish_notation = "2 3 - | 5 *"
+    rev_polish_notation = "2 3 - abs 5 *"
     r = construct_expr_tree(rev_polish_notation)
     # Display infix notation
     print("Infix expression is:")
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     print("\nEvaluation: ", eval_expr_tree(r))
 
     # test 2
-    rev_polish_notation = "TP(0) TP(1) - |"
+    rev_polish_notation = "TP(0) TP(1) - abs"
     r = construct_expr_tree(rev_polish_notation)
     # Display infix notation
     print("Infix expression is:")
@@ -23,3 +23,31 @@ if __name__ == "__main__":
     predicted_Y = pd.Series(np.array([1, 1, 1, 1, 1, 1]))
     T = pd.Series(np.array([0, 1, 0, 1, 0, 1]))
     print("\nEvaluation: ", eval_expr_tree(r, Y, predicted_Y, T))
+    # Evaluate conf interval for TP(0)
+    delta = 0.05
+    l_t_test, u_t_test = eval_func_bound("TP(0)", Y, predicted_Y, T, delta,
+                                         Inequality.T_TEST)
+    l_hoeffding, u_hoeffding = eval_func_bound("TP(0)", Y, predicted_Y, T,
+                                               delta, Inequality.HOEFFDING_INEQUALITY)
+    print("Confidence Interval for TP(0):")
+    print(f" T test: [{l_t_test}, {u_t_test}]")
+    print(f" Hoeffding Inequality: [{l_hoeffding}, {u_hoeffding}]")
+    # Evaluate conf interval for TP(1)
+    l_t_test, u_t_test = eval_func_bound("TP(1)", Y, predicted_Y, T, delta,
+                                           Inequality.T_TEST)
+    l_hoeffding, u_hoeffding = eval_func_bound("TP(1)", Y, predicted_Y, T,
+                                                 delta, Inequality.HOEFFDING_INEQUALITY)
+    print("Confidence Interval for TP(1):")
+    print(f" T test: [{l_t_test}, {u_t_test}]")
+    print(f" Hoeffding Inequality: [{l_hoeffding}, {u_hoeffding}]")
+    # Evaluate conf interval
+    l_t_test, u_t_test = eval_expr_tree_conf_interval(r, Y, predicted_Y, T, delta,
+                                           Inequality.T_TEST)
+    l_hoeffding, u_hoeffding = eval_expr_tree_conf_interval(r, Y, predicted_Y, T,
+                                                 delta, Inequality.HOEFFDING_INEQUALITY)
+    print("Confidence Interval for expression tree:")
+    print(f" T test: [{l_t_test}, {u_t_test}]")
+    print(f" Hoeffding Inequality: [{l_hoeffding}, {u_hoeffding}]")
+
+
+
